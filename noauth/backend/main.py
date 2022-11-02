5f542a811081db6a9b3ce9dbb48634ff943f8b8a
Kimitoshi Takahashi <ktaka@ccmp.jp>
2022-11-02 11:13:51 +0900

    wip

--- noauth/backend/main.py
+++ noauth/backend/main.py
@@ -2,8 +2,16 @@ from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 from db import Customer, CustomerBase, SessionLocal
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+        return {"token": token}
 
 origins = [
     "http://localhost:3000",
